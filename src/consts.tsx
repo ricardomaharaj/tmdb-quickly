3b7e1f308e4239528b410ee58e2ae1df1bd9d73e
Ricardo Maharaj <ricardorobinmaharaj@gmail.com>
2022-08-31 23:33:47 -0400

    remove imgurl for object of urls

--- src/consts.tsx
+++ src/consts.tsx
@@ -1,5 +1,10 @@
-export const IMGURL = 'https://image.tmdb.org/t/p/w500'
-export const FULLIMGURL = 'https://image.tmdb.org/t/p/original'
+export const IMG_URLs = {
+    ORIGINAL: 'https://image.tmdb.org/t/p/original',
+    W500: 'https://image.tmdb.org/t/p/w500',
+    W94H141: 'https://www.themoviedb.org/t/p/w94_and_h141_bestv2',
+    W150H225: 'https://www.themoviedb.org/t/p/w150_and_h225_bestv2',
+    W227H127: 'https://www.themoviedb.org/t/p/w227_and_h127_bestv2'
+}
 
 export interface State {
     query: string
