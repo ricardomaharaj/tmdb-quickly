2719a08a0656b9ebb6fb006cd7b1d063f321884e
Ricardo Maharaj <ricardorobinmaharaj@gmail.com>
2022-09-29 17:22:29 -0400

    add placeholder profile images

--- src/Season.tsx
+++ src/Season.tsx
@@ -121,7 +121,7 @@ export function Season({ state, updateState }: Props) {
                             className='flex flex-row bg-slate-800 rounded-xl p-2 hover:bg-slate-700'
                             key={i}
                         >
-                            {x.profile_path && (
+                            {x.profile_path ? (
                                 <img
                                     src={`${IMG_URLs.W94H141}${x.profile_path}`}
                                     className='rounded-xl mr-2 max-w-[94px] max-h-[141px]'
@@ -130,6 +130,19 @@ export function Season({ state, updateState }: Props) {
                                     height='141'
                                     alt=''
                                 />
+                            ) : (
+                                <div className='bg-slate-900 rounded-xl mr-2'>
+                                    <svg
+                                        xmlns='http://www.w3.org/2000/svg'
+                                        width='16'
+                                        height='16'
+                                        fill='currentColor'
+                                        className='w-[94px] h-[141px] brightness-50'
+                                        viewBox='0 0 16 16'
+                                    >
+                                        <path d='M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z' />
+                                    </svg>
+                                </div>
                             )}
                             <div>
                                 {x.name && <div>{x.name}</div>}
@@ -151,7 +164,7 @@ export function Season({ state, updateState }: Props) {
                             className='flex flex-row bg-slate-800 rounded-xl p-2 hover:bg-slate-700'
                             key={i}
                         >
-                            {x.profile_path && (
+                            {x.profile_path ? (
                                 <img
                                     src={`${IMG_URLs.W94H141}${x.profile_path}`}
                                     className='rounded-xl mr-2 max-w-[94px] max-h-[141px]'
@@ -160,6 +173,19 @@ export function Season({ state, updateState }: Props) {
                                     height='141'
                                     alt=''
                                 />
+                            ) : (
+                                <div className='bg-slate-900 rounded-xl mr-2'>
+                                    <svg
+                                        xmlns='http://www.w3.org/2000/svg'
+                                        width='16'
+                                        height='16'
+                                        fill='currentColor'
+                                        className='w-[94px] h-[141px] brightness-50'
+                                        viewBox='0 0 16 16'
+                                    >
+                                        <path d='M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z' />
+                                    </svg>
+                                </div>
                             )}
                             <div>
                                 {x.name && <div>{x.name}</div>}
