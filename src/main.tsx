909b284636734b1f60c9c9579ba9f61457a4092b
Ricardo Maharaj <ricardorobinmaharaj@gmail.com>
2022-11-16 23:21:17 -0500

    rename index to main

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,5 @@
 import { createRoot } from 'react-dom/client'
 import { App } from './App'
-import { registerSW } from './SWRegister'
 import './style.css'
 
 createRoot(document.querySelector('#root')!).render(<App />)
-
-registerSW()
