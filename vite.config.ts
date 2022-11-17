627dd7335119b2f640611b44b3fd085ae594f7b9
Ricardo Maharaj <ricardorobinmaharaj@gmail.com>
2022-11-16 23:25:10 -0500

    move from react-scripts to vite

---
+++ vite.config.ts
@@ -0,0 +1,43 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
+
+export default defineConfig({
+    plugins: [
+        react(),
+        VitePWA({
+            manifest: {
+                name: 'TMDB Quickly',
+                short_name: 'TMDB Quickly',
+                description: 'TMDB Quickly',
+                icons: [
+                    {
+                        src: 'logo16.png',
+                        sizes: '16x16',
+                        type: 'image/png'
+                    },
+                    {
+                        src: 'logo32.png',
+                        sizes: '32x32',
+                        type: 'image/png'
+                    },
+                    {
+                        src: 'logo192.png',
+                        type: 'image/png',
+                        sizes: '192x192'
+                    },
+                    {
+                        src: 'logo512.png',
+                        type: 'image/png',
+                        sizes: '512x512'
+                    }
+                ],
+                start_url: '.',
+                display: 'standalone',
+                theme_color: '#000000',
+                background_color: '#000000'
+            },
+            registerType: 'autoUpdate'
+        })
+    ]
+})
