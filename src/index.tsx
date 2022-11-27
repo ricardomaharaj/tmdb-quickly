614acb4905b318d228438c2fc796e0bac5270439
Ricardo Maharaj <ricardorobinmaharaj@gmail.com>
2022-11-26 23:33:52 -0500

    rename

--- src/index.tsx
+++ src/index.tsx
@@ -2,4 +2,4 @@ import { createRoot } from 'react-dom/client'
 import { App } from './App'
 import './style.css'
 
-createRoot(document.getElementById('app')!).render(<App />)
+createRoot(document.getElementById('root')!).render(<App />)
