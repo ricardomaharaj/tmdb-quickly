c94e47e4a2723f7f2fa8f517514529caabc3d919
Ricardo Maharaj <ricardorobinmaharaj@gmail.com>
2023-04-02 22:06:52 -0400

    replace header and menu

---
+++ src/comps/header.tsx
@@ -0,0 +1,9 @@
+import Link from 'next/link'
+
+export function Header() {
+  return (
+    <Link href='/' className='row justify-center'>
+      <div className='text-3xl'>T M D B | N E X T</div>
+    </Link>
+  )
+}
