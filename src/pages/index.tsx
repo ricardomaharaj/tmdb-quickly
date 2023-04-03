879c4a9fd2286e7a0e711c9085edbbf65a0e04e9
Ricardo Maharaj <ricardorobinmaharaj@gmail.com>
2023-04-02 22:10:01 -0400

    add query param support and pagination

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,70 +1,98 @@
-import { useGlobalCtx } from "@/state/global"
-import { SearchResult, SearchResults } from "@/types/search-results"
-import { overviewTrimmer } from "@/util"
-import { api } from "@/util/local-api"
-import { useEffect, useState } from "react"
+import Card from '@/comps/card'
+import { SearchResults } from '@/types/search-results'
+import { trimmer } from '@/util'
+import { api } from '@/util/local-api'
+import { useRouter } from 'next/router'
+import { useEffect, useRef, useState } from 'react'
 
 export default function Home() {
-  const [results, setResults] = useState<SearchResults>()
+  const router = useRouter()
 
-  const ctx = useGlobalCtx()
-  const { query, setQuery, page, setPage } = ctx.search
+  const query = (router.query.query as string) || ''
+  const page = parseInt(router.query.page as string) || 1
 
+  const { data: results } = useSearch({ query, page })
+
+  function updateQueries(update: any) {
+    router.replace({ query: { query, page, ...update } })
+  }
+
+  const [debounce, setDebounce] = useState(query)
+  const ref = useRef<NodeJS.Timeout>()
   useEffect(() => {
-    setPage(1)
-    api
-      .get<SearchResults>("search", { query, page: page.toString() })
-      .then((x) => setResults(x))
-  }, [query])
+    ref.current = setTimeout(() => {
+      updateQueries({ query: debounce })
+    }, 500)
+    return () => clearTimeout(ref.current)
+  }, [debounce])
 
   return (
     <>
-      {!query && (
-        <div className="col m-2">
-          <div className="row justify-center">
-            <div className="text-lg">Trending This Week</div>
-          </div>
+      <div className='row'>
+        <input
+          type='text'
+          placeholder='SEARCH'
+          defaultValue={query}
+          className='w-full border-2 p-2 text-center outline-none'
+          onChange={(e) => setDebounce(e.target.value)}
+        />
+      </div>
+      <div className='grid123'>
+        {results?.results?.map(
+          ({
+            poster_path,
+            profile_path,
+            title,
+            name,
+            release_date,
+            first_air_date,
+            media_type,
+            overview,
+            id,
+          }) => (
+            <Card
+              img={poster_path || profile_path}
+              primary={title || name}
+              secondary={query && (release_date || first_air_date)}
+              tertiary={overview && trimmer(overview)}
+              path={`${media_type}/${id}`}
+              key={`${media_type}/${id}`}
+            />
+          )
+        )}
+      </div>
+      {query && (
+        <div className='row justify-evenly py-2'>
+          <button
+            className='px-4'
+            onClick={() => updateQueries({ page: page - 1 })}
+          >
+            {'<'}
+          </button>
+          <div>{page}</div>
+          <button
+            className='px-4'
+            onClick={() => updateQueries({ page: page + 1 })}
+          >
+            {'>'}
+          </button>
         </div>
       )}
-      <div className="col mx-2 mt-4 space-y-3">
-        {results?.results?.map((result) => (
-          <SearchResultCard result={result} key={result.id} />
-        ))}
-      </div>
     </>
   )
 }
 
-function SearchResultCard(props: { result: SearchResult }) {
-  const { result } = props
-  const { title, name } = result
-  const { overview } = result
-  const { poster_path } = result
+function useSearch(args: { query: string; page: number }) {
+  const { query, page } = args
 
-  return (
-    <>
-      <div className="row rounded-xl">
-        {poster_path ? (
-          <div className="col">
-            <img
-              src={`https://image.tmdb.org/t/p/w500/${result.poster_path}`}
-              className="max-w-[100px] rounded-l-xl"
-              loading="lazy"
-              alt=""
-            />
-          </div>
-        ) : (
-          <div className="col h-[150px] w-[100px] rounded-l-xl bg-black"></div>
-        )}
-        <div className="col rounded-r-xl border border-l-0 border-black">
-          <div className="col ml-1 mt-1 p-1">
-            <div className="font-bold">{title || name}</div>
-            {overview && (
-              <div className="italic">{overviewTrimmer(overview)}</div>
-            )}
-          </div>
-        </div>
-      </div>
-    </>
-  )
+  const [data, setData] = useState<SearchResults>()
+
+  useEffect(() => {
+    api
+      .get<SearchResults>('search', { query, page })
+      .then((res) => setData(res))
+      .catch((e) => console.log(e))
+  }, [query, page])
+
+  return { data }
 }
