2bc8e0df550e6533b9c91d7ec6ad6edd68f1381d
Ricardo Maharaj <ricardorobinmaharaj@gmail.com>
2023-04-03 21:25:34 -0400

    refactor api folder

--- src/pages/api/movie.ts
+++ src/pages/api/movie.ts
@@ -2,8 +2,8 @@ import { tmdbApi } from '@/util/tmdb-api'
 import { NextApiHandler } from 'next'
 
 const handler: NextApiHandler = async (req, res) => {
-  const { query, page } = req.query as Record<string, string>
-  const x = await tmdbApi.search({ query, page })
+  const { id } = req.query as Record<string, string>
+  const x = await tmdbApi.movieCredits({ id })
   return res.json(x)
 }
 
