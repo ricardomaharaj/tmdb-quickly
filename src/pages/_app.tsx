41220d86934e3ece8978a545376e0cfee165b2f7
Ricardo Maharaj <ricardorobinmaharaj@gmail.com>
2023-04-05 19:45:35 -0400

    add urql

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,25 @@
-import '@/styles/globals.css'
-import { Header } from '@/comps/header'
 import type { AppProps } from 'next/app'
+import '~/styles/globals.css'
+
+import { Header } from '~/comps/header'
+import * as Urql from 'urql'
+
+const url = '/api/gql'
+
+const UrqlClient = Urql.createClient({
+  url,
+  exchanges: [Urql.cacheExchange, Urql.fetchExchange],
+})
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <div className='col m-2 space-y-2'>
-        <Header />
-        <Component {...pageProps} />
-      </div>
+      <Urql.Provider value={UrqlClient}>
+        <div className='col m-2'>
+          <Header />
+          <Component {...pageProps} />
+        </div>
+      </Urql.Provider>
     </>
   )
 }
