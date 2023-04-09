764413dac277b4d64d2ef7659cc57b71caf9e133
Ricardo Maharaj <ricardorobinmaharaj@gmail.com>
2023-04-09 15:57:52 -0400

    responsive text

--- src/comps/header.tsx
+++ src/comps/header.tsx
@@ -2,8 +2,8 @@ import Link from 'next/link'
 
 export function Header() {
   return (
-    <Link href='/' className='row justify-center'>
-      <div className='text-3xl'>T M D B | N E X T</div>
+    <Link href='/' className='row my-2 justify-center'>
+      <div className='text-3xl md:text-4xl xl:text-5xl'>T M D B | N E X T</div>
     </Link>
   )
 }
