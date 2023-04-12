848372333eeda577321acb16e62df3f4b49f75c5
Ricardo Maharaj <ricardorobinmaharaj@gmail.com>
2023-04-12 02:16:07 -0400

    update prettier config and plugins

---
+++ prettier.config.cjs
@@ -0,0 +1,8 @@
+/** @type {import('prettier').Config} */
+module.exports = {
+  semi: false,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  trailingComma: 'none',
+  plugins: ['prettier-plugin-tailwindcss', 'prettier-plugin-organize-imports']
+}
