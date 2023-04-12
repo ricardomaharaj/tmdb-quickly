d5e08dbcb36b696293abe238531aa83c05f9f9d9
Ricardo Maharaj <ricardorobinmaharaj@gmail.com>
2023-04-11 23:26:20 -0400

    add additional image sizes

--- src/consts.ts
+++ src/consts.ts
@@ -1,4 +1,6 @@
 export const imageUrls = {
+  original: 'https://image.tmdb.org/t/p/original',
+  w500: 'https://image.tmdb.org/t/p/w500',
   w94h141: 'https://image.tmdb.org/t/p/w94_and_h141_bestv2',
 }
 
