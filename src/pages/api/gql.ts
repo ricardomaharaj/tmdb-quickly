ce9024ef91d65b863876f826e7f39d8ade92d097
Ricardo Maharaj <ricardorobinmaharaj@gmail.com>
2023-05-26 02:14:56 -0400

    add server side filtering and pagination

--- src/pages/api/gql.ts
+++ src/pages/api/gql.ts
@@ -12,8 +12,39 @@ const schema = createSchema<Yoga>({
         if (!query) return await TMDB.trending()
         return await TMDB.search({ query, page })
       },
-      movie: async (_, { id }: { id: ID }) => {
-        return await TMDB.movie({ id })
+      movie: async (
+        _,
+        { id, query = '', page = 1 }: { id: ID; query?: string; page?: number },
+      ) => {
+        const movie = await TMDB.movie({ id })
+
+        const perPage = 9
+        const start = (page - 1) * perPage
+        const end = page * perPage
+
+        if (movie?.credits?.cast) {
+          let cast = movie.credits.cast
+          if (query) {
+            cast = cast.filter((x) =>
+              JSON.stringify(x).toLowerCase().includes(query),
+            )
+          }
+          cast = cast.slice(start, end)
+          movie.credits.cast = cast
+        }
+
+        if (movie?.credits?.crew) {
+          let crew = movie?.credits?.crew
+          if (query) {
+            crew = crew.filter((x) =>
+              JSON.stringify(x).toLowerCase().includes(query),
+            )
+          }
+          crew = crew.slice(start, end)
+          movie.credits.crew = crew
+        }
+
+        return movie
       },
       tv: async (_, { id }: { id: ID }) => {
         return await TMDB.tv({ id })
