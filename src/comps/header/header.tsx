47b533424e942779c10816178486a2a8d9481401
Ricardo Maharaj <ricardorobinmaharaj@gmail.com>
2023-05-27 03:22:53 -0400

    add header

---
+++ src/comps/header/header.tsx
@@ -0,0 +1,9 @@
+import Link from 'next/link'
+
+export function Header() {
+  return (
+    <div className='row mx-2 my-4 justify-center text-2xl'>
+      <Link href='/'>T M D B | N E X T</Link>
+    </div>
+  )
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,12 @@
 import type { AppProps } from 'next/app'
+import { Header } from '~/comps/header'
 import '~/styles/globals.css'
 import { Urql } from '~/util/urql'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Urql>
+      <Header />
       <Component {...pageProps} />
     </Urql>
   )
