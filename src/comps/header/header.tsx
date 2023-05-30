3d245dee53fefe5f0076489bc75b1807fdbb8bd0
Ricardo Maharaj <ricardorobinmaharaj@gmail.com>
2023-05-30 01:59:37 -0400

    make entire header a link

--- src/comps/header/header.tsx
+++ src/comps/header/header.tsx
@@ -2,8 +2,8 @@ import Link from 'next/link'
 
 export function Header() {
   return (
-    <div className='row mx-2 my-4 justify-center text-2xl'>
-      <Link href='/'>T M D B | N E X T</Link>
-    </div>
+    <Link href='/' className='row mx-2 my-4 justify-center text-2xl'>
+      <div>T M D B | N E X T</div>
+    </Link>
   )
 }
