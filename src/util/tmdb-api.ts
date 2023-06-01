48933ca11dfe74a13d51d4485bc90ca305c7bae1
Ricardo Maharaj <ricardorobinmaharaj@gmail.com>
2023-06-01 19:58:23 -0400

    use arg type

--- src/util/tmdb-api.ts
+++ src/util/tmdb-api.ts
@@ -1,5 +1,5 @@
 import { env } from '~/env'
-import { ID } from '~/types/id'
+import { Args } from '~/types/args'
 import { Episode, Movie, Search, Season, TV } from '~/types/tmdb'
 import { Fetcher } from '~/util/fetcher'
 
@@ -12,33 +12,33 @@ export const TMDB = {
   trending: async () => {
     return await api.get<Search>('/trending/all/week')
   },
-  search: async ({ query, page }: { query: string; page: number }) => {
+  search: async (args: Pick<Args, 'query' | 'page'>) => {
+    const { query, page } = args
     return await api.get<Search>('/search/multi', { query, page })
   },
-  movie: async ({ id }: { id: ID }) => {
+  movie: async (args: Pick<Args, 'id'>) => {
+    const { id } = args
     return await api.get<Movie>(`/movie/${id}`, {
       append_to_response: 'credits,images,videos,release_dates,',
     })
   },
-  tv: async ({ id }: { id: ID }) => {
+  tv: async (args: Pick<Args, 'id'>) => {
+    const { id } = args
     return await api.get<TV>(`/tv/${id}`, {
       append_to_response: 'aggregate_credits,images,videos',
     })
   },
-  season: async ({ id, season_number }: { id: ID; season_number: number }) => {
+  season: async (args: Pick<Args, 'id' | 'season_number'>) => {
+    const { id, season_number } = args
     return await api.get<Season>(`/tv/${id}/season/${season_number}`, {
       append_to_response: 'credits,images,videos',
     })
   },
-  episode: async ({
-    id,
-    season_number,
-    episode_number,
-  }: {
-    id: ID
-    season_number: number
-    episode_number: number
-  }) => {
+  episode: async (
+    args: Pick<Args, 'id' | 'season_number' | 'episode_number'>,
+  ) => {
+    const { id, season_number, episode_number } = args
+
     return await api.get<Episode>(
       `/tv/${id}/season/${season_number}/episode/${episode_number}`,
       {
