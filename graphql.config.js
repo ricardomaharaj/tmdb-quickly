c383bb731f04038e7d37262bcdcda982dea57a8d
Ricardo Maharaj <ricardorobinmaharaj@gmail.com>
2023-06-08 22:01:22 -0400

    config updates

---
+++ graphql.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  schema: './gql/schema.gql',
+}
---
+++ prettier.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  semi: false,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  trailingComma: 'all',
+  plugins: ['prettier-plugin-organize-imports', 'prettier-plugin-tailwindcss'],
+}
