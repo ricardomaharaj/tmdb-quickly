55fca53c84277d32a6ddc08a660e1bea14e0cb49
Ricardo Maharaj <ricardorobinmaharaj@gmail.com>
2023-08-21 01:58:31 -0400

    update import and usage

--- src/pages/api/gql.ts
+++ src/pages/api/gql.ts
@@ -1,10 +1,10 @@
 import { readFileSync } from 'fs'
 import { createSchema, createYoga } from 'graphql-yoga'
-import { tmdbResolvers } from '~/server/util/tmdb'
+import { tmdbResolvers } from '~/server/gql/resolvers'
 
 const schema = createSchema({
   typeDefs: readFileSync('./gql/schema.gql').toString('utf-8'),
-  resolvers: { Query: { ...tmdbResolvers } },
+  resolvers: { Query: tmdbResolvers },
 })
 
 const yoga = createYoga({
