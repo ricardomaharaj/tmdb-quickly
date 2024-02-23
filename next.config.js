a5896d011ec96fb1e5f992c9d4f03f8cb7be03d0
Ricardo Maharaj <ricardorobinmaharaj@gmail.com>
2024-02-23 01:41:32 -0500

    update configs

--- next.config.js
+++ next.config.js
@@ -8,6 +8,14 @@ const withPWA = require('next-pwa')({
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  output: 'standalone',
+  images: {
+    unoptimized: true,
+    disableStaticImages: true,
+  },
+  experimental: {
+    scrollRestoration: true,
+  },
 }
 
 module.exports = withPWA(nextConfig)
