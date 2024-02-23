c73564b47236d17b5344ef9f2ec4c48263d1958f
Ricardo Maharaj <ricardorobinmaharaj@gmail.com>
2024-02-23 01:34:50 -0500

    add caching

--- src/server/gql/yoga.ts
+++ src/server/gql/yoga.ts
@@ -1,3 +1,4 @@
+import { useResponseCache as responseCache } from '@graphql-yoga/plugin-response-cache'
 import { createYoga } from 'graphql-yoga'
 import { schema } from './schema'
 
@@ -5,4 +6,9 @@ export const yoga = createYoga({
   schema: schema,
   graphqlEndpoint: '/api/gql',
   graphiql: process.env.NODE_ENV === 'development',
+  plugins: [
+    responseCache({
+      session: () => null,
+    }),
+  ],
 })
